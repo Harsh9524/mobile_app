97a341a11aeb97b13fcfa1c2de456e5c39e29cb4
Harsh9524 <harshpanwar9524@gmail.com>
2020-05-11 01:17:24 +0530

    added drawef

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,48 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          backgroundColor: Colors.black54,
-          elevation: 0.0,
-          title: Image.asset(
-            'assets/images/logo.png',
-            height: 150,
-            width: 200,
-          )),
+        backgroundColor: Colors.black54,
+        elevation: 0.0,
+        title: Image.asset(
+          'assets/images/logo.png',
+          height: 100,
+          width: 200,
+        ),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('School'),
+              padding: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.black26,
+              ),
+            ),
+            ListTile(
+              title: Text('Assignment'),
+              
+
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                // Then close the drawer.
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Events'),
+              onTap: () {
+                // Update the state of the app.
+                // ...
+                // Then close the drawer.
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
